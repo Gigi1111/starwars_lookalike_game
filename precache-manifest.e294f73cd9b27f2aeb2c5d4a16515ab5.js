da2a07c79f3ae88a06f709deb7352a634ff2b02a
Gigi1111 <airolg1111@gmail.com>
2019-09-06 01:55:18 +0800

    Updates

---
+++ precache-manifest.e294f73cd9b27f2aeb2c5d4a16515ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e315d27617b9b7f8a5aae384baddd8c",
+    "url": "/Gigi1111/starwars_lookalike_game/index.html"
+  },
+  {
+    "revision": "1e7892f40d07716ec8da",
+    "url": "/Gigi1111/starwars_lookalike_game/static/css/main.1fceec57.chunk.css"
+  },
+  {
+    "revision": "b972c8a2c09df9f39869",
+    "url": "/Gigi1111/starwars_lookalike_game/static/js/2.8bccc198.chunk.js"
+  },
+  {
+    "revision": "1e7892f40d07716ec8da",
+    "url": "/Gigi1111/starwars_lookalike_game/static/js/main.65e5190f.chunk.js"
+  },
+  {
+    "revision": "5f0c29a9b8dab0152660",
+    "url": "/Gigi1111/starwars_lookalike_game/static/js/runtime~main.41dcb24b.js"
+  }
+]);
